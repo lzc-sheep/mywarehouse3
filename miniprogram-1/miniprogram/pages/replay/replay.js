e2d66b273483ff9112c70edabac8af778f054a84
Nora Song <clown12@sjtu.edu.cn>
2020-04-26 19:12:32 +0800

    add replay.js

--- miniprogram-1/miniprogram/pages/replay/replay.js
+++ miniprogram-1/miniprogram/pages/replay/replay.js
@@ -1,66 +1,75 @@
-// pages/replay/replay.js
+var that
+const db = wx.cloud.database();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    id: '',
+    openid: '',
+    content: '',
+    user: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    that = this;
+    that.jugdeUserLogin();
+    that.data.id = options.id;
+    that.data.openid = options.openid;
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  bindKeyInput(e) {
+    that.data.content = e.detail.value;
+    console.log("内容：" + that.data.content)
 
   },
+  jugdeUserLogin: function(event) {
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+              that.data.user = res.userInfo;
+              console.log(that.data.user)
+            }
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  saveReplay: function() {
+    db.collection('replay').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        content: that.data.content,
+        date: new Date(),
+        r_id: that.data.id,
+        u_id: that.data.openid,
+        t_id: that.data.id,
+        user: that.data.user,
+      },
+      success: function(res) {
+        wx.showToast({
+          title: '发射成功',
+        })
+        setTimeout(function() {
+          wx.navigateBack({
+            url: "../homeDetail/homeDetail?id=" + that.data.id + "&openid=" + that.data.openid
+          })
+        }, 1500)
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      },
+      fail: console.error
+    })
   }
+
 })
