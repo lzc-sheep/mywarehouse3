dc56480163e47495d93de7a0c230b2fc3d1a4a7d
one-cup-sugar <1422363784@sjtu.edu.cn>
2020-04-27 10:07:52 +0800

    上传me文件

--- miniprogram-1/miniprogram/pages/me/me.js
+++ miniprogram-1/miniprogram/pages/me/me.js
@@ -1,6 +1,18 @@
-// pages/me/me.js
+// miniprogram/pages/me/me.js
+var app = getApp();
 Page({
 
+  actioncnt: function() {        
+    wx.showActionSheet({            
+      itemList:  ['群聊',  '好友',  '朋友圈'],
+      success: function(res)  {
+        console.log(res.tapIndex)
+      },
+      fail: function(res)  {
+        console.log(res.errMsg)
+      }
+    })   
+  },
   /**
    * 页面的初始数据
    */
@@ -11,56 +23,46 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+  },
+  /**
+   * 收藏列表
+   */
+  onCollectClick:function(event){
+    wx.navigateTo({
+      url: '../collect/collect',
+    })
+  },
+  /**
+   * 发布历史
+   */
+  onHistoryClick:function(event){
+    wx.navigateTo({
+      url: '../history/history',
+    })
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 提交意见
    */
-  onReady: function () {
-
+  onAdvanceClick:function(event){
+    wx.navigateTo({
+      url: '../advance/advance',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  clickParticipation: function(event) {
+    wx.navigateTo({
+      url: '../participation/participation',
+    })
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(event) {
+    console.log(event);
   }
 })
