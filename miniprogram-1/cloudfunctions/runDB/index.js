57514082ac8caa515b5cbea1b99b131434912c9f
lzc <2015267341@qq.com>
2020-05-09 17:42:17 +0800

    runDB的部署

---
+++ miniprogram-1/cloudfunctions/runDB/index.js
@@ -0,0 +1,43 @@
+
+// runDB云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+const _ = db.command
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const targetDB = db.collection(event.db)
+  try {
+    
+    if (event.type == "insert") {
+    
+      return await targetDB.add({
+        data: event.data,
+        success: res => {
+          console.log("添加纪录成功", res);
+        },
+        fail: res => {
+          console.log("添加纪录失败", res);
+        }
+      })
+    }
+
+    if (event.type == "update") {
+      return await targetDB.doc(event.indexKey).update({
+        data: event.data
+      })
+    }
+
+    if (event.type == "delete") {
+      return await targetDB.doc(event.indexKey).remove()
+    }
+
+    if (event.type == "get") {
+      return await targetDB.where(event.condition).get()
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
